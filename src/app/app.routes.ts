2a86930579b917fb9cf59c9283a3d5bf89e4079b
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-07-18 01:19:00 +0700

    feat(init): init components, ui & pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent } from './shared/layouts/default/default.component';
+import { MasterComponent } from './shared/layouts/master/master.component';
+import { TodoComponent } from './pages/todo/todo.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [{ path: '', component: LoginComponent }],
+  },
+  {
+    path: '',
+    component: MasterComponent,
+    children: [{ path: 'todo', component: TodoComponent }],
+  },
+];
