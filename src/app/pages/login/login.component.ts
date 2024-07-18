f0556b75cf59ae34f14913c2eb7b8f0264d6ab63
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-07-18 07:49:58 +0700

    feat: add login form component with validation
    - Implemented login form with email and password fields
    - Added reactive form validation (required, email, password complexity)
    - Included dynamic validation feedback (valid/invalid states)
    - Styled form with Tailwind CSS and custom styles
    - Integrated icons for password field validation
    - Improved user experience with tooltip display on password field

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule], // Import CommonModule here
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+  showTooltip: boolean = false;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          '(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}'
+        ),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+      console.error('Form is invalid');
+    }
+  }
 }
