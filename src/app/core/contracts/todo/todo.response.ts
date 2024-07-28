e3c2b897e25bcbdfe784322bc42b670e0344646e
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-07-28 08:58:26 +0700

    feat: add mapping todo response to todo model

--- src/app/core/contracts/todo/todo.response.ts
+++ src/app/core/contracts/todo/todo.response.ts
@@ -1,3 +1,6 @@
+import { TodoStatus } from "../../enums";
+import { ITodo } from "../../models";
+
 export interface TodoResponse {
   id: number;
   title: string;
@@ -7,4 +10,35 @@ export interface TodoResponse {
   created: Date;
   lastModifiedBy: string;
   lastModified: Date | null;
+}
+export function mapTodoResponseToITodo(response: TodoResponse): ITodo {
+  return {
+    id: response.id,
+    title: response.title,
+    description: response.description,
+    status: mapStatus(response.status),
+    createdBy: response.createdBy,
+    created: new Date(response.created),
+    lastModifiedBy: response.lastModifiedBy,
+    lastModified: response.lastModified
+      ? new Date(response.lastModified)
+      : undefined,
+  };
+}
+
+export function mapStatus(status: number): TodoStatus {
+  switch (status) {
+    case 1:
+      return TodoStatus.Opening;
+    case 2:
+      return TodoStatus.Progressing;
+    case 3:
+      return TodoStatus.Testing;
+    case 4:
+      return TodoStatus.Done;
+    case 5:
+      return TodoStatus.Rejected;
+    default:
+      throw new Error(`Unknown status: ${status}`);
+  }
 }
