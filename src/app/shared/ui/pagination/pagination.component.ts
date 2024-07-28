e769eb18ab2ea40a177bd11770e435c5ec36ef3b
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-07-28 08:59:05 +0700

    feat(components): add pagination & todo card

---
+++ src/app/shared/ui/pagination/pagination.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class PaginationComponent {
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 1;
+  @Input() pageSize: number = 10;
+  @Input() totalRecords: number = 0;
+
+  @Output() pageChange = new EventEmitter<number>();
+
+  get pages(): number[] {
+    const pages = [];
+    for (let i = 1; i <= this.totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }
+
+  onPageChange(page: number): void {
+    if (page > 0 && page <= this.totalPages) {
+      this.pageChange.emit(page);
+    }
+  }
+}
