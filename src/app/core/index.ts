5f9806d066e7a8b18a3f06b7fd22e269b6195699
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-07-28 08:59:56 +0700

    feat(todo): add load todo list view

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,5 +1,6 @@
 export * from './contants';
 export * from './contracts';
 export * from './enums';
+export * from './interceptors';
 export * from './models';
 export * from './services';
--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../core/services/todo.service'; // Adjust import path
+import { TodoCardComponent } from '../../shared/components/todo-card/todo-card.component';
+import { ITodo } from '../../core';
+import { CommonModule } from '@angular/common';
+import { PaginationComponent } from '../../shared/ui/pagination/pagination.component';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [TodoCardComponent, PaginationComponent, CommonModule],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss'
+  styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  todos: ITodo[] | null = [];
 
+  currentPage: number = 1;
+  totalPages: number = 1;
+  pageSize: number = 10;
+  totalRecords: number = 0;
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.loadTodos(this.currentPage, this.pageSize);
+  }
+
+  loadTodos(page: number, pageSize: number): void {
+    this.todoService.getAllTodos(page, pageSize).subscribe({
+      next: (response) => {
+        if (response.data) {
+          this.todos = response.data;
+          this.currentPage = response.pageNumber;
+          this.totalPages = response.totalPages;
+          this.totalRecords = response.totalRecords;
+        }
+      },
+      error: (err) => {
+        console.error('Error loading todos', err);
+      },
+    });
+  }
+
+  onPageChange(page: number): void {
+    if (page > 0 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.loadTodos(this.currentPage, this.pageSize);
+    }
+  }
 }
